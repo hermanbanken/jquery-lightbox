8e2f2c5e17bf835c177acbe0a94bb60945987e2c
Herman <hermanbanken@gmail.com>
2011-06-08 17:45:14 +0200

    Rewritten lightbox2.php. The ugly code is now replaced by a beautiful class.
    Signed-off-by: Herman <hermanbanken@gmail.com>

--- lightbox2.php
+++ lightbox2.php
@@ -8,74 +8,190 @@ Author: Herman Banken
 Author URI: http://hermanbanken.nl/
 */
 
-/* Where our theme reside: */
-$lightbox_jq_theme_path = (dirname(__FILE__)."/Themes");
-update_option('lightbox_jq_theme_path', $lightbox_jq_theme_path);
-/* Set the default theme to Black */
-add_option('lightbox_jq_theme', 'Black');
-add_option('lightbox_jq_automate', 1);
-add_option('lightbox_jq_resize_on_demand', 0);
+class LightboxJQ {
+	public $plugin_path = "";
+	public $theme = "Black";
+	public $automate = true;
+	public $resize = false;
+	public $optionspage = 'lightboxjq';
+	
+	public function __construct(){
+		add_action('init', array($this, 'init'));
+		add_action('wp_head', array($this, 'add_style'));
+		add_action('wp_head', array($this, 'lighter'));
+		add_action('admin_menu', array($this, 'menu'));	
+	}
+	
+	public function init(){
+		$this->get_vars();
+		
+		// Register filters and scripts
+		$jsfile = plugins_url( "jquery.lightbox.js", __FILE__);
+		wp_register_script( 'lightbox-jq', $jsfile, array('jquery'), '2.9' );
 
-/* use WP_PLUGIN_URL if version of WP >= 2.6.0. If earlier, use wp_url */
-if($wp_version >= '2.6.0') {
-	$jq_lightbox_plugin_prefix = WP_PLUGIN_URL."/jquery-lightbox/"; /* plugins dir can be anywhere after WP2.6 */
-} else {
-	$jq_lightbox_plugin_prefix = get_bloginfo('wpurl')."/wp-content/plugins/jquery-lightbox/";
+		if (!is_admin()) { // if we are viewing an admin page, don't do lightboxing:
+			wp_enqueue_script('lightbox-jq');
+		}
+		
+		if($this->automate){
+			add_filter('the_content', array($this, 'auto_lightbox_filter'), 99);
+			add_filter('the_excerpt', array($this, 'auto_lightbox_filter'), 99);
+		}
+	}
+	
+	private function get_vars(){
+		// Fetch variables
+		$this->plugin_path = dirname(__FILE__);
+		$this->theme = get_option('lightbox_jq_theme');
+		$this->automate = get_option('lightbox_jq_automate');
+		$this->resize = get_option('lightbox_jq_resize');
+	}
+
+	public function menu(){
+		add_options_page( 
+			'Lightbox JQ Options', 'Lightbox JQ', 
+			'manage_options', $this->optionspage, array($this, 'options_page')
+		);
+	}
+	
+	/*
+	* Added a code to automatically insert rel="lightbox[nameofpost]" 
+	* to every image with no manual work. If there are already 
+	* rel="lightbox[something]" attributes, they are not clobbered.
+	*  
+    * Michael Tyson, you are a regular expressions god! ;) 
+    * http://atastypixel.com
+	*/
+	public function auto_lightbox_filter($content) {
+		global $post;
+		$pattern        = "/(<a(?![^>]*?rel=['\"]lightbox.*)[^>]*".
+						  "?href=['\"][^'\"]+?\.(?:bmp|gif|jpg|jpeg|png)['\"][^\>]*)>/i";
+		$replacement    = '$1 rel="lightbox['.$post->ID.']">';
+		$content = preg_replace($pattern, $replacement, $content);
+		return $content;
+	}
+	
+	public function add_style(){
+		$sheet = plugins_url( "Themes/".$this->theme."/lightbox.css", __FILE__);
+		echo "<link rel='stylesheet' href='".$sheet."' type='text/css' media='screen' />";
+	}
+	
+	private function available_themes(){
+		/* Check if there are themes: */
+		$path =  $this->plugin_path . "/Themes";
+		$themes = array();
+		//print_r($lightbox_2_theme_path);
+		if ($handle = opendir($path)) {
+		    while (false !== ($file = readdir($handle))) {
+		        if (substr($file, 0, 1) != "." && file_exists($path."/".$file."/lightbox.css")) {
+		            $themes[$file] = $path."/".$file."/";
+		        }   
+		    }
+		    closedir($handle);
+		}
+		return $themes;
+	}
+	
+	public function lighter(){ ?><script type="text/javascript">
+		jQuery(document).ready(function(){
+			var rels = {};
+			jQuery("a[rel^=lightbox]").lightbox({
+					fitToScreen: true,
+			    	scaleImages: true,
+			    	xScale: 1.2,
+			    	yScale: 1.2,
+			    	displayDownloadLink: true,
+			    	imageClickClose: false		
+			});
+			/*for(rel in rels){
+				console.log(jQuery("a").filter(function(){
+					return (jQuery(this).attr("rel") == rel);
+				}).lightbox({
+					fitToScreen: true,
+			    	scaleImages: true,
+			    	xScale: 1.2,
+			    	yScale: 1.2,
+			    	displayDownloadLink: true,
+			    	imageClickClose: false		
+				}));
+			}*/
+		});
+	</script><?php }
+		
+	public function options_page(){
+		if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['lightbox_jq_theme'])){
+			update_option('lightbox_jq_theme', $_POST['lightbox_jq_theme']);
+			update_option('lightbox_jq_automate', $_POST['lightbox_jq_automate']);
+			update_option('lightbox_jq_resize', $_POST['lightbox_jq_resize']);
+			$this->get_vars();
+			echo "<h1>Updating vars</h1>";
+		}
+		
+		$action = admin_url("options-general.php?page=".$this->optionspage);
+		?>
+		<div class='wrap'>
+			<h2><?php _e('Lightbox 2 Options', 'lightbox_jq') ?></h2>
+			<form name='lightbox-jq-options' method='post' action='<?php echo $action; ?>'>
+				<table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+					<tr valign="baseline">
+					<th scope="row"><?php _e('Lightbox Appearance', 'lightbox_jq') ?></th> 
+					<td><select name='lightbox_jq_theme'>
+					<?php
+						$current_theme = get_option('lightbox_jq_theme');
+						foreach($this->available_themes() as $theme => $dir){
+							$selected = $theme == $this->theme ? 'selected' : '';
+							echo "<option value='".urlencode($theme)."' $selected>$theme</option>";
+						}
+					?>
+					</select><p><small><?php 
+						_e('If in doubt, try the Black theme', 'lightbox_jq') 
+					?></small></p></td></tr>
+					<tr valign="baseline">
+					<th scope="row"><?php 
+						_e('Auto-lightbox image links', 'lightbox_jq') ?></th> 
+			    	<td><?php
+						$ca = $this->automate ? 'checked' : '';
+			    		echo "<input type='checkbox' name='lightbox_jq_automate' value='1' $ca />";
+			    	?><p><small><?php 
+						_e('Let the plugin add necessary html to image links', 'lightbox_jq') 
+					?></small></p>
+				 	</td>
+					<tr valign="baseline">
+					<th scope="row"><?php 
+						_e('Shrink large images to fit smaller screens', 'lightbox_jq') ?></th> 
+					<td><?php
+						$cr = $this->resize ? 'checked' : '';
+				   		echo "<input type='checkbox' name='lightbox_jq_resize' value='1' $cr />";
+				   	?><p><small><?php 
+						_e('Note: <u>Excessively large images</u> waste bandwidth '.
+						   'and slow browsing!', 'lightbox_jq');
+					?></small></p>
+					</td>
+			    	</tr>
+				</table>
+
+				<p class="submit">
+			   		<input type="submit" value="<?php _e('Save Changes', 'lightbox_jq') ?>" />
+			    </p>
+			</form>
+		</div>
+	<?php }
 }
 
-/* options page (required for saving prefs)*/
-$options_page = get_option('siteurl') . '/wp-admin/admin.php?page=lightbox-jq/options.php';
-/* Adds our admin options under "Options" */
-function lightbox_jq_options_page() {
-	add_options_page('Lightbox Options', 'Lightbox 2', 10, 'lightbox-jq/options.php');
+/*
+var rels = {};
+jQuery("a[rel^=lightbox]").each(function(){ rels[jQuery(this).attr('rel')] = true; });
+for(rel in rels){
+	jQuery("a[rel="+rel+"]").lightbox({
+		fitToScreen: true,
+	    scaleImages: true,
+	    xScale: 1.2,
+	    yScale: 1.2,
+	    displayDownloadLink: true,
+	    imageClickClose: false		
+	});
 }
-
-function jq_lightbox_styles() {
-	/* What version of WP is running? */
-	global $wp_version;
-	global $jq_lightbox_plugin_prefix;
-    /* The next line figures out where the javascripts and images and CSS are installed,
-    relative to your wordpress server's root: */
-    $lightbox_jq_theme = urldecode(get_option('lightbox_jq_theme'));
-    $lightbox_style = ($jq_lightbox_plugin_prefix."Themes/".$lightbox_jq_theme."/");
-
-    /* The xhtml header code needed for lightbox to work: */
-	$lightboxscript = "
-	<!-- begin lightbox scripts -->
-	<script type=\"text/javascript\">
-    //<![CDATA[
-    document.write('<link rel=\"stylesheet\" href=\"".$lightbox_style."lightbox.css\" type=\"text/css\" media=\"screen\" />');
-    //]]>
-    </script>
-	<!-- end lightbox scripts -->\n";
-	/* Output $lightboxscript as text for our web pages: */
-	echo($lightboxscript);
-}
-
-/* Added a code to automatically insert rel="lightbox[nameofpost]" to every image with no manual work. 
-If there are already rel="lightbox[something]" attributes, they are not clobbered. 
-Michael Tyson, you are a regular expressions god! ;) 
-http://atastypixel.com
 */
-function jq_autoexpand_rel_wlightbox ($content) {
-	global $post;
-	$pattern        = "/(<a(?![^>]*?rel=['\"]lightbox.*)[^>]*?href=['\"][^'\"]+?\.(?:bmp|gif|jpg|jpeg|png)['\"][^\>]*)>/i";
-	$replacement    = '$1 rel="lightbox['.$post->ID.']">';
-	$content = preg_replace($pattern, $replacement, $content);
-	return $content;
-}
 
-if (get_option('lightbox_jq_automate') == 1){
-	add_filter('the_content', 'jq_autoexpand_rel_wlightbox', 99);
-	add_filter('the_excerpt', 'jq_autoexpand_rel_wlightbox', 99);
-}
-
-wp_register_script( 'lightbox-jq', $jq_lightbox_plugin_prefix.'lightbox.js', array('jquery'), '2.9' );
-
-if (!is_admin()) { // if we are *not* viewing an admin page, like writing a post or making a page:
-	wp_enqueue_script('lightbox-jq');
-
-/* we want to add the above xhtml to the header of our pages: */
-add_action('wp_head', 'jq_lightbox_styles');
-add_action('admin_menu', 'lightbox_jq_options_page');
+new LightboxJQ();
 ?>
